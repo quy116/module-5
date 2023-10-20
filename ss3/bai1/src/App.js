408cecb49f7482e90fcc9e91073dc37cc3550630
quy116 <nguyenphucquy1106@gmail.com>
2023-10-20 11:46:24 +0700

    hi

--- ss3/bai1/src/App.js
+++ ss3/bai1/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import StudentInfoComponent from "./student/StudentInfoComponent";
+import StudentInfoComponent from "./components/StudentInfoComponent";
 
 function App() {
   return (
